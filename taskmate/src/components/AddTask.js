ce8a6c6ad97c9772ecc1dc1b6b1a527753a4d4b9
Sourabhkumbh <Sourabh.Kumbhar@emerson.com>
2024-04-18 16:49:52 +0530

    error

--- taskmate/src/components/AddTask.js
+++ taskmate/src/components/AddTask.js
@@ -1,6 +1,8 @@
 import React from "react";
+import { useState } from "react";
 
 function AddTask({ taskList, setTaskList, task, setTask }) {
+  const [error, setError] = useState(false)
   function handleSubmit(e) {
     if (task.id) {
       e.preventDefault();
@@ -12,18 +14,25 @@ function AddTask({ taskList, setTaskList, task, setTask }) {
       setTaskList(updatedObj);
 
     } else {
-      e.preventDefault();
-      const date = new Date();
-      console.log(e.target.task.value);
-      const newTask = {
-        id: date.getTime(),
-        time: `${date.toLocaleTimeString()} ${date.toLocaleDateString()}`,
-        name: e.target.task.value,
-      };
-      setTaskList([...taskList, newTask]);
-      e.target.task.value = "";
-      task.name = '';
-    }
+      if(e.target.task.value === ""){
+        e.preventDefault();
+        setError(true);        
+      }else{
+        e.preventDefault();
+        const date = new Date();
+        console.log(e.target.task.value);
+        const newTask = {
+          id: date.getTime(),
+          time: `${date.toLocaleTimeString()} ${date.toLocaleDateString()}`,
+          name: e.target.task.value,
+        };
+        setTaskList([...taskList, newTask]);
+        e.target.task.value = "";
+        task.name = '';
+        setError(false);
+      }
+      }
+      
   }
   return (
     <section className="addTask">
@@ -36,8 +45,10 @@ function AddTask({ taskList, setTaskList, task, setTask }) {
           placeholder="add task"
           onChange={e=> setTask({...task, name : e.target.value})}
         />
+        
         <button type="submit">{task.id ? "Update" : "Add"}</button>
       </form>
+      {error ? <p style={{color :'red'}}>write something please!!</p> : ""}
     </section>
   );
 }
